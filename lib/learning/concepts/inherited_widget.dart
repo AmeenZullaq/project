38c9e6d57ab493b2a89984be64911c57c4a9a13b
ameen <ameenzullaq@gmail.com>
2025-06-01 15:53:55 +0300

    refactor inherited widget

--- lib/learning/concepts/inherited_widget.dart
+++ lib/learning/concepts/inherited_widget.dart
@@ -1,21 +1,22 @@
 import 'package:flutter/material.dart';
 
 /// inherited widget is a way to efficiently pass data down the widget tree.
-class MyInherited extends InheritedWidget {
-  const MyInherited({super.key, required super.child});
+class ContaienrTheme extends InheritedWidget {
+  const ContaienrTheme({super.key, required super.child});
 
   final Color color = Colors.red;
+  final double size = 100;
 
-  static MyInherited of(BuildContext context) {
-    return context.dependOnInheritedWidgetOfExactType<MyInherited>()!;
+  static ContaienrTheme of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ContaienrTheme>()!;
   }
 
   @override
-  bool updateShouldNotify(covariant MyInherited oldWidget) {
+  bool updateShouldNotify(covariant ContaienrTheme oldWidget) {
     /// it will notify only when the color changes
     /// oldWidget means the widget before the update and newWidget means the widget after the update
     /// when return true it will notify and rebuild the widget and when return false it will not notify and not rebuild
-    return oldWidget.color != color;
+    return oldWidget.color != color || oldWidget.size != size;
   }
 }
 
@@ -26,31 +27,18 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: MyInherited(
+        body: ContaienrTheme(
           child: Center(
-            child: ColoredBox(),
+            child: Builder(builder: (context) {
+              return Container(
+                height: ContaienrTheme.of(context).size,
+                width: ContaienrTheme.of(context).size,
+                color: ContaienrTheme.of(context).color,
+              );
+            }),
           ),
         ),
       ),
-    );
-  }
-}
-
-class ColoredBox extends StatelessWidget {
-  const ColoredBox({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    /// that hard because each time i want to create a copy of ColoredBox i need o defin that line, so i take it and out in the MyInherited inside of method
-    // MyInherited myInherited =
-    //     context.dependOnInheritedWidgetOfExactType<MyInherited>()!;
-
-    return Container(
-      height: 200,
-      width: 200,
-      color: MyInherited.of(context).color,
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,75 @@
-import 'package:easy_localization/easy_localization.dart';
-import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:project/core/services/shared_preferences.dart';
-import 'package:project/core/theme/app_themes.dart';
-import 'package:project/core/theme/cubit/theme_cubit.dart';
+// import 'package:easy_localization/easy_localization.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_bloc/flutter_bloc.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+// import 'package:hydrated_bloc/hydrated_bloc.dart';
+// import 'package:path_provider/path_provider.dart';
+// import 'package:project/core/services/shared_preferences.dart';
+// import 'package:project/core/theme/app_themes.dart';
+// import 'package:project/core/theme/cubit/theme_cubit.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await SharedPrefs.init();
-  await EasyLocalization.ensureInitialized();
-  HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory: kIsWeb
-        ? HydratedStorage.webStorageDirectory
-        : await getApplicationDocumentsDirectory(),
-  );
-  runApp(
-    EasyLocalization(
-      supportedLocales: const [Locale('en'), Locale('ar')],
-      path: 'assets/translations',
-      startLocale: const Locale('en'),
-      fallbackLocale: const Locale('en'),
-      child: const MyApp(),
-    ),
-  );
-}
+// void main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await SharedPrefs.init();
+//   await EasyLocalization.ensureInitialized();
+//   HydratedBloc.storage = await HydratedStorage.build(
+//     storageDirectory: kIsWeb
+//         ? HydratedStorage.webStorageDirectory
+//         : await getApplicationDocumentsDirectory(),
+//   );
+//   runApp(
+//     EasyLocalization(
+//       supportedLocales: const [Locale('en'), Locale('ar')],
+//       path: 'assets/translations',
+//       startLocale: const Locale('en'),
+//       fallbackLocale: const Locale('en'),
+//       child: const MyApp(),
+//     ),
+//   );
+// }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
 
-  final designSize = const Size(360, 690);
+//   final designSize = const Size(360, 690);
 
-  @override
-  Widget build(BuildContext context) {
-    return ScreenUtilInit(
-      designSize: designSize,
-      minTextAdapt: true,
-      splitScreenMode: true,
-      fontSizeResolver: (fontSize, instance) {
-        final display = View.of(context).display;
-        final screenSize = display.size / display.devicePixelRatio;
-        final scaleWidth = screenSize.width / designSize.width;
-        return fontSize * scaleWidth;
-      },
-      builder: (_, __) {
-        return MultiBlocProvider(
-          providers: [
-            BlocProvider(
-              create: (context) => ThemeCubit(),
-            ),
-          ],
-          child: BlocBuilder<ThemeCubit, ThemeMode>(
-            builder: (context, newMode) {
-              return MaterialApp(
-                debugShowCheckedModeBanner: false,
-                localizationsDelegates: context.localizationDelegates,
-                supportedLocales: context.supportedLocales,
-                locale: context.locale,
-                theme: AppThemes.lightTheme,
-                darkTheme: AppThemes.darkTheme,
-                themeMode: newMode,
-                home: const Scaffold(),
-                // routerConfig: RouterService().router,
-              );
-            },
-          ),
-        );
-      },
-    );
-  }
-}
-
-
-
-
-
+//   @override
+//   Widget build(BuildContext context) {
+//     return ScreenUtilInit(
+//       designSize: designSize,
+//       minTextAdapt: true,
+//       splitScreenMode: true,
+//       fontSizeResolver: (fontSize, instance) {
+//         final display = View.of(context).display;
+//         final screenSize = display.size / display.devicePixelRatio;
+//         final scaleWidth = screenSize.width / designSize.width;
+//         return fontSize * scaleWidth;
+//       },
+//       builder: (_, __) {
+//         return MultiBlocProvider(
+//           providers: [
+//             BlocProvider(
+//               create: (context) => ThemeCubit(),
+//             ),
+//           ],
+//           child: BlocBuilder<ThemeCubit, ThemeMode>(
+//             builder: (context, newMode) {
+//               return MaterialApp(
+//                 debugShowCheckedModeBanner: false,
+//                 localizationsDelegates: context.localizationDelegates,
+//                 supportedLocales: context.supportedLocales,
+//                 locale: context.locale,
+//                 theme: AppThemes.lightTheme,
+//                 darkTheme: AppThemes.darkTheme,
+//                 themeMode: newMode,
+//                 home: const Scaffold(),
+//                 // routerConfig: RouterService().router,
+//               );
+//             },
+//           ),
+//         );
+//       },
+//     );
+//   }
+// }
